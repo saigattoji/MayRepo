8eee0ee362f8972b18da3dc1f1330133d38c832e
saigattoji <137893682+saigattoji@users.noreply.github.com>
2024-05-29 16:57:01 +0530

    Update Javacode.java

--- Javacode.java
+++ Javacode.java
@@ -3,7 +3,7 @@ import java.util.*
 public class void main(string[] str){
 
 static void xyz(int a, int b)
-system.out.println("changes done");
+system.out.println("changes done by developers");
 }
 
 
